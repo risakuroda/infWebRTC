6ad14c02ae44bfed614b996431e8cc8c1a981f39
yuuri <yuuri.sb@gmail.com>
2023-07-05 13:43:32 +0900

    videochat

---
+++ src/videochat-top.js
@@ -0,0 +1,26 @@
+const music = document.getElementById('music');
+const movie = document.getElementById('movie');
+const youtube = document.getElementById('youtube');
+const divination = document.getElementById('divination');
+const karaoke = document.getElementById('karaoke');
+
+music.onclick = function nextpage(){
+    var roomNameInput = music.value;
+    window.open(`videochat-room.html?value=${roomNameInput}`);
+};
+movie.onclick = function nextpage(){
+    var roomNameInput = movie.value;
+    window.open(`videochat-room.html?value=${roomNameInput}`);
+};
+youtube.onclick = function nextpage(){
+    var roomNameInput = movie.value;
+    window.open(`videochat-room.html?value=${roomNameInput}`);
+};
+divination.onclick = function nextpage(){
+    var roomNameInput = movie.value;
+    window.open(`videochat-room.html?value=${roomNameInput}`);
+};
+karaoke.onclick = function nextpage(){
+    var roomNameInput = movie.value;
+    window.open(`videochat-room.html?value=${roomNameInput}`);
+};
